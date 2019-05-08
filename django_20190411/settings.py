1567144ff4be35ec0c19f7d30ded38490dac4502
gorvernment2048@gmail.com <orivicky@gmail.com>
2019-05-08 16:15:41 +0900

    modify setting

--- django_20190411/settings.py
+++ django_20190411/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Monkey-Patching
+import sys
+if 'win' not in sys.platform and 'darwin' not in sys.platform:
+    import django_heroku
+    django_heroku.settings(locals())
